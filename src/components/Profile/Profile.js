5a2dde818c2303ad46e2c818bb31826c408438bb
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-08-24 12:25:51 +0300

    inline style

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,3 +1,6 @@
+import PropTypes from 'prop-types';
+import urlPropType from 'url-prop-type';
+import Wrapper from './Wrapper.styled';
 const Profile = ({
   avatar,
   username,
@@ -14,21 +17,31 @@ const Profile = ({
       <p class="tag">{tag}</p>
       <p class="location">{location}</p>
     </div>
-
-    <ul class="stats">
-      <li>
-        <span class="label">Followers</span>
-        <span class="quantity">{followers}</span>
-      </li>
-      <li>
-        <span class="label">Views</span>
-        <span class="quantity">{views}</span>
-      </li>
-      <li>
-        <span class="label">Likes</span>
-        <span class="quantity">{likes}</span>
-      </li>
-    </ul>
+    <Wrapper>
+      <ul class="stats" style={{ listStyleType: 'none' }}>
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">{followers}</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">{views}</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">{likes}</span>
+        </li>
+      </ul>
+    </Wrapper>
   </div>
 );
 export default Profile;
+Profile.propTypes = {
+  avatar: urlPropType.isRequired,
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  followers: PropTypes.number.isRequired,
+  views: PropTypes.number.isRequired,
+  likes: PropTypes.number.isRequired,
+};
---
+++ src/components/Profile/Wrapper.styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+const Wrapper = styled.div`
+  display: inline-flex;
+  align-items: center;
+  gap: 16px;
+  padding: 8px;
+  border: 1px solid black;
+  border-radius: 8px;
+  background-color: rebeccapurple;
+`;
+export default Wrapper;
