ea72719de1bcda5c72fe73a13db24f6b8398ea0d
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-08-30 13:27:42 +0300

    stat bgColor Random done

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,25 +1,21 @@
 import PropTypes from 'prop-types';
 import styles from './Statistics.module.css';
 import data from 'components/Statistics/data';
-// const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-// document.body.style.backgroundColor = '#' + randomColor;
-const MyComponents = {
-  DatePicker: function DatePicker(props) {
-    return <div>Imagine a {props.color} datepicker here.</div>;
-  },
-};
-function BlueDatePicker() {
-  return <MyComponents.DatePicker color="blue" />;
-}
+
 const statList = data.map(({ id, label, percentage }) => {
+  const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
   return (
-    <li key={id} className={styles.item}>
+    <li
+      key={id}
+      className={styles.item}
+      style={{ backgroundColor: randomColor }}
+    >
       <span className={styles.label}>{label}: </span>
       <span className={styles.percentage}>{percentage}%</span>
     </li>
   );
 });
-export const Statistics = ({ title }) => (
+export const Statistics = ({ title } ) => (
   <section className={styles.statistics}>
     <h2 className={styles.title}>{title}</h2>
 
