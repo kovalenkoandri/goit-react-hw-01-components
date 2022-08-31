54c02508b84eaeb178804e1c9cebc0b3ef073b3a
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-08-31 20:46:43 +0300

    items mapped

--- src/components/TransactionHistory/TransactionHistory.js
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -1,9 +1,5 @@
-// import items from './transactions';
 import styles from './TransactionHistory.module.css';
-const TransactionHistory = ({ items }) => {
-  // items.map(({ id, type, amount, currency }) => {
-  // console.log(id, type, amount, currency);
-
+const TransactionHistory = ({items}) => {
   return (
     <table className={styles.transactionHistory}>
       <thead className={styles.thead}>
@@ -13,23 +9,15 @@ const TransactionHistory = ({ items }) => {
           <th>Currency</th>
         </tr>
       </thead>
-
-      <tbody key={items[0].id} className={styles.tbody}>
-        {console.log(items[0].id)}
-        <tr>
-          <td>{items[0].type}</td>
-          <td>{items[0].amount}</td>
-          <td>{items[0].currency}</td>
-        </tr>
-      </tbody>
-      <tbody key={items[1].id} className={styles.tbody}>
-        {console.log(items[1].id)}
-        <tr>
-          <td>{items[1].type}</td>
-          <td>{items[1].amount}</td>
-          <td>{items[1].currency}</td>
-        </tr>
-      </tbody>
+      {items.map(({ id, type, amount, currency }) => (
+        <tbody key={id} className={styles.tbody}>
+          <tr>
+            <td>{type}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        </tbody>
+      ))}
     </table>
   );
 };
