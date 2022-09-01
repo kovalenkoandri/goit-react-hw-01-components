68274b269a1980f7ea7502966f182c1ce1825f62
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-09-01 13:18:12 +0300

    propTypes Statistics title checking

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,11 +1,17 @@
 import styles from './Statistics.module.css';
 import StatList from './statList';
+import PropTypes from 'prop-types';
 
 const Statistics = ({ title, stats }) => (
   <section className={styles.statistics}>
     {title && <h2 className={styles.title}>{title}</h2>}
 
-    <ul className={styles.statList}><StatList stats={stats} /></ul>
+    <ul className={styles.statList}>
+      <StatList stats={stats} />
+    </ul>
   </section>
 );
 export default Statistics;
+Statistics.propTypes = {
+  title: PropTypes.string,
+};
